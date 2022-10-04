4b0fb9ce3a1a77159508179de46cf1c234b87bf1
KyoshiNoda <kyoshisew@gmail.com>
2022-10-04 11:39:29 -0400

    added merge two linked lists

--- solutions/merge-two-sorted-lists.py
+++ solutions/merge-two-sorted-lists.py
@@ -6,7 +6,6 @@ class ListNode:
   def __init__(self,val = 0, next = None) -> None:
     self.val = val
     self.next = next
-``
 class Solution:
   def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     dummyNode = ListNode()
